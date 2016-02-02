e49091985a5f5aadac5966eddf1ea2bb651a54ca
Greg J <gjjeffers@gmail.com>
2016-02-02 17:55:38 -0500

    Added NodeGroup, NodeGroupTest, and two more test puzzles.

---
+++ SudokuSolver2/SudokuSolver2.Tests/Models/NodeGroupTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuSolver2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver2.Models.Tests
+{
+    [TestClass]
+    public class NodeGroupTests
+    {
+        #region MethodTests
+        [TestMethod]
+        public void PossibilityCountTest()
+        {
+            List<Node> possTestNodes = new List<Node>();
+            possTestNodes.Add(new Node());
+            possTestNodes.Add(new Node());
+            possTestNodes.Add(new Node());
+
+            NodeGroup possTest = new NodeGroup(possTestNodes);
+            possTest.Nodes[0].SetValue(1);
+            Assert.AreEqual(0, possTest.PossibilityCount(1));
+            Assert.AreEqual(2, possTest.PossibilityCount(2));
+
+        }
+
+        [TestMethod]
+        public void ValueSetEventTest()
+        {
+            List<Node> valueTestNodes = new List<Node>();
+            valueTestNodes.Add(new Node());
+            valueTestNodes.Add(new Node());
+
+            NodeGroup valueTest = new NodeGroup(valueTestNodes);
+            valueTest.Nodes[0].SetValue(1);
+            Assert.IsFalse(valueTest.Nodes[0].Possibilities.Contains(1));
+            Assert.IsFalse(valueTest.Nodes[1].Possibilities.Contains(1));
+        }
+
+        [TestMethod]
+        public void SolveTest()
+        {
+            List<Node> solveTestNodes = new List<Node>();
+            for(int i = 0; i < 9; i++)
+            {
+                solveTestNodes.Add(new Node());
+
+            }
+            solveTestNodes[0].Possibilities = new List<int>() { 1 };
+            solveTestNodes[1].Possibilities = new List<int>() { 1, 2 };
+            solveTestNodes[2].Possibilities = new List<int>() { 1, 2, 3 };
+            solveTestNodes[3].Possibilities = new List<int>() { 1, 3, 4 };
+            solveTestNodes[4].Possibilities = new List<int>() { 1, 4, 5 };
+            solveTestNodes[5].Possibilities = new List<int>() { 2, 5, 6 };
+            solveTestNodes[6].Possibilities = new List<int>() { 2, 6, 7 };
+            solveTestNodes[7].Possibilities = new List<int>() { 2, 7, 8 };
+            solveTestNodes[8].Possibilities = new List<int>() { 2, 8, 9 };
+
+            NodeGroup solveTest = new NodeGroup(solveTestNodes);
+            solveTest.Solve();
+            Assert.AreEqual(1, solveTest.Nodes[0].Value);
+            Assert.AreEqual(2, solveTest.Nodes[1].Value);
+            Assert.AreEqual(3, solveTest.Nodes[2].Value);
+            Assert.AreEqual(4, solveTest.Nodes[3].Value);
+            Assert.AreEqual(5, solveTest.Nodes[4].Value);
+            Assert.AreEqual(6, solveTest.Nodes[5].Value);
+            Assert.AreEqual(7, solveTest.Nodes[6].Value);
+            Assert.AreEqual(8, solveTest.Nodes[7].Value);
+            Assert.AreEqual(9, solveTest.Nodes[8].Value);
+        }
+        #endregion
+    }
+}
---
+++ SudokuSolver2/SudokuSolver2/Models/NodeGroup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace SudokuSolver2.Models
+{
+    public class NodeGroup: INotifyPropertyChanged
+    {
+        // collection for nodes representing row, column, and block. manages possibilities among member nodes as values become set. 
+        // examines member nodes for singular possibilities, if found sets corresponding node's value
+        public List<Node> Nodes;
+        private bool _busy = false;
+        public bool Busy  //flag to notify listeners (Puzzle) that activity is currently happening in the NodeGroup
+        {
+            get { return _busy; }
+            set
+            {
+                _busy = value;
+                NotifyPropertyChanged("Busy");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public NodeGroup(List<Node> NodePool)
+        {
+            Nodes = NodePool;
+            // assign delegate to listen for values being set on member nodes and remove established values as possibilities from other member nodes
+            foreach (Node n in Nodes) 
+            {
+                n.PropertyChanged += delegate (object sender, PropertyChangedEventArgs e)
+                {
+                    Busy = true;
+                    foreach (Node node in Nodes)
+                    {
+                        node.Possibilities.Remove(((Node)sender).Value);                        
+                    }
+                    Busy = false;
+                };
+            }
+        }
+
+        public int PossibilityCount(int poss)
+        {
+            // finds the number of occurances of poss in member nodes
+            // a return of 1 indicates a singular value that can be set
+            int _possCount = 0;
+            foreach(Node n in Nodes)
+            {
+                if (n.Possibilities.Contains(poss))
+                {
+                    _possCount++;
+                }
+            }
+            return _possCount;
+        }
+
+        public void Solve()
+        {
+            // examines member nodes for singular values and sets the value if found
+
+            // examine each member node for only having one possibility 
+            foreach(Node x in Nodes)
+            {
+                if(x.Possibilities.Count == 1)
+                {
+                    x.SetValue(x.Possibilities[0]);
+                }
+            }
+
+            // examine all member nodes for a possibility count of 1
+            for(int i = 1; i < 10; i++)
+            {
+                if(PossibilityCount(i) == 1)
+                {
+                    foreach(Node n in Nodes)
+                    {
+                        if (n.Possibilities.Contains(i))
+                        {
+                            n.SetValue(i);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+
+    }
+}
